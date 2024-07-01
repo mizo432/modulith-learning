a1d249299cb5abc5c5a0ace5e4e441e610c38e65
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2024-07-01 19:24:45 +0900

    InvoiceRegistrationNumberクラスを更新
    InvoiceRegistrationNumberクラスにコメントを追加し、`NonNull`のインポートを削除しました。具体的には、クラスを作成および再構築する方法について詳細な説明を提供するためにコメントを追加しました。また、不必要なインポートを削除してクラスの整理を改善しました。

--- backend/src/main/java/undecided/erp/relationship/domain/model/party/party/InvoiceRegistrationNumber.java
+++ backend/src/main/java/undecided/erp/relationship/domain/model/party/party/InvoiceRegistrationNumber.java
@@ -6,7 +6,6 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 import lombok.Setter;
 import undecided.erp.common.primitive.Objects2;
 
@@ -19,10 +18,32 @@ import undecided.erp.common.primitive.Objects2;
 @Setter
 public class InvoiceRegistrationNumber {
 
+  /**
+   * 空のInvoiceRegistrationNumberオブジェクトを表します。
+   * <p>
+   * InvoiceRegistrationNumberオブジェクトは、資格を持つ請求書発行オペレータの登録番号を表します。
+   * その値がnullの場合、InvoiceRegistrationNumberオブジェクトは空とみなされます。
+   * <p>
+   * このstatic final変数にアクセスすることで、空のInvoiceRegistrationNumberオブジェクトを取得できます。
+   *
+   * @see InvoiceRegistrationNumber
+   */
   public static final InvoiceRegistrationNumber EMPTY = new InvoiceRegistrationNumber();
+
+  /**
+   * InvoiceRegistrationNumberオブジェクトのシリアライズされた値。
+   */
   @JsonValue
   private String value;
 
+  /**
+   * 提供された値に基づいてInvoiceRegistrationNumberオブジェクトを作成します。
+   * <p>
+   * もし値がnullであれば、InvoiceRegistrationNumber.EMPTYを返します。
+   *
+   * @param value InvoiceRegistrationNumberを作成するために使用される値
+   * @return 作成されたInvoiceRegistrationNumber、または値がnullの場合はInvoiceRegistrationNumber.EMPTY
+   */
   @JsonCreator
   public static InvoiceRegistrationNumber of(String value) {
     return Objects2.nonNull(value) ?
@@ -34,15 +55,19 @@ public class InvoiceRegistrationNumber {
    * 提供された値とタイプに基づいてInvoiceRegistrationNumberを再構築します。
    *
    * @param value InvoiceRegistrationNumberを再構築するために使用される値
-   * @param type 請求書登録の対象性を決定するPartyType
    * @return 再構築されたInvoiceRegistrationNumber、値がnullの場合はInvoiceRegistrationNumber.EMPTYを返します
    */
-  public static InvoiceRegistrationNumber reconstruct(String value, @NonNull PartyType type) {
+  public static InvoiceRegistrationNumber reconstruct(String value) {
     return Objects2.nonNull(value) ?
         new InvoiceRegistrationNumber(value)
         : InvoiceRegistrationNumber.EMPTY;
   }
 
+  /**
+   * オブジェクトの文字列表現を返します。
+   *
+   * @return オブジェクトの文字列表現。
+   */
   @Override
   public String toString() {
     return String.valueOf(value);
