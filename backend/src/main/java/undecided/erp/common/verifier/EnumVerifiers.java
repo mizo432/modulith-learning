62afe98e4e23ae5b2c30df32294fd61a9318f3d0
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2024-07-02 10:31:05 +0900

    EnumVerifiersの検証機能を改善
    EnumVerifiersクラスのメソッドが列挙型の定数がEnumSetに含まれていないことを確認する機能と、テスト値が基準値と等しくないことを検証する機能を追加・修正しました。これにより、より緻密な検証が可能になりました。

--- backend/src/main/java/undecided/erp/common/verifier/EnumVerifiers.java
+++ backend/src/main/java/undecided/erp/common/verifier/EnumVerifiers.java
@@ -1,5 +1,7 @@
 package undecided.erp.common.verifier;
 
+import static undecided.erp.common.primitive.Objects2.isNull;
+
 import java.util.EnumSet;
 import java.util.function.Supplier;
 import lombok.NonNull;
@@ -26,19 +28,47 @@ public class EnumVerifiers {
   }
 
   /**
-   * 引数として与えられたtestValueがreferenceValueと等しくないことを確認します。
+   * 指定された列挙型の定数が与えられたEnumSetに含まれていないことを確認します。
    * <p>
-   * もし等しければ、指定されたsupplierを使って例外をスローします。
+   * もし含まれている場合、指定された例外がスローされます。
    *
-   * @param referenceValue 比較するための参照値。
-   * @param testValue 参照値との非等価性を確認するための値。
-   * @param exceptionSupplier 値が等しい場合にスローされる例外を提供します。
-   * @param <E> Enumの種類を示します。
-   * @return 与えられたtestValueがそれと等しくない場合、参照値を返します。
-   * @throws RuntimeException 与えられたtestValueが参照値と等しい場合。
+   * @param ref 列挙型の定数の参照
+   * @param enumSet 確認するEnumSet
+   * @param supplier 列挙型の定数が含まれている場合にスローする例外を提供するサプライヤー
+   * @param <EnumType> 列挙型のタイプ
+   * @param <ExceptionType> 例外のタイプ
+   * @return EnumSetに列挙型の定数が含まれていない場合、同じ列挙型の定数
+   * @throws RuntimeException 列挙型の定数がEnumSetに含まれている場合
    */
-  public static <E extends Enum<E>> E verifyNotEqual(E referenceValue, @NonNull E testValue,
-      @NonNull Supplier<? extends RuntimeException> exceptionSupplier) {
+  public static <EnumType extends Enum<EnumType>, ExceptionType extends RuntimeException> EnumType verifyNotContains(
+      EnumType ref, @NonNull EnumSet<EnumType> enumSet,
+      @NonNull Supplier<ExceptionType> supplier) {
+    if (isNull(ref)) {
+      return null;
+    }
+    if (enumSet.contains(ref)) {
+      throw supplier.get();
+    }
+    return ref;
+
+  }
+
+  /**
+   * テスト値が基準値と等しくないことを検証します。
+   * <p>
+   * これらが等しい場合、例外がスローされます。
+   *
+   * @param referenceValue 比較のための基準値
+   * @param testValue 比較のためのテスト値
+   * @param exceptionSupplier 値が等しい場合にスローされる例外を提供するサプライヤー
+   * @param <EnumType> Enumの型
+   * @param <ExceptionType> スローされる例外の型
+   * @return テスト値が基準値と等しくない場合の基準値
+   * @throws ExceptionType テスト値が基準値と等しい場合
+   */
+  public static <EnumType extends Enum<EnumType>, ExceptionType extends RuntimeException> EnumType verifyNotEqual(
+      EnumType referenceValue, @NonNull EnumType testValue,
+      @NonNull Supplier<ExceptionType> exceptionSupplier) {
     if (testValue == referenceValue) {
       throw exceptionSupplier.get();
     }
