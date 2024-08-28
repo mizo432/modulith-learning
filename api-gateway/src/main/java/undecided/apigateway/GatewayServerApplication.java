cd4f198326ee6a24857509f16e86edc35ce86c08
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2024-08-29 04:45:13 +0900

    Add custom route configuration to GatewayServerApplication
    新たにRouteLocator Beanを追加し、特定のパスに対してバックエンドサービスへのルーティングを設定しました。これにより、/backbone/**パスへのリクエストが適切なマイクロサービスに転送されます。

--- api-gateway/src/main/java/undecided/apigateway/GatewayServerApplication.java
+++ api-gateway/src/main/java/undecided/apigateway/GatewayServerApplication.java
@@ -3,6 +3,9 @@ package undecided.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +15,27 @@ public class GatewayServerApplication {
     SpringApplication.run(GatewayServerApplication.class, args);
   }
 
+  /**
+   * ゲートウェイサーバーでリクエストをルーティングするためのカスタムなRouteLocatorを返します。
+   *
+   * @param builder RouteLocatorを構築するために使用されるRouteLocatorBuilder
+   * @return カスタムなルートが設定されたRouteLocator
+   */
+  @Bean
+  public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+    return builder.routes()
+        .route("backbone", r -> r.path("/backbone/**")
+//            .filters(f -> f.stripPrefix( 1 ))
+            .uri("lb://BACKBONE"))
+/*
+        .route("service1", r -> r.path("/service1/**")
+            .filters(f -> f.stripPrefix(1))
+            .uri("lb://SERVICE-NAME1"))
+        .route("service2", r -> r.path("/service2/**")
+            .filters(f -> f.stripPrefix(1))
+            .uri("lb://SERVICE-NAME2"))
+
+ */
+        .build();
+  }
 }
