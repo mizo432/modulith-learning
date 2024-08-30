91cc1b269c33ea16c5674ebac0388c71891c5130
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2024-08-30 11:23:59 +0900

    ドキュメント追加: EurekaServerApplicationクラスにJavadocを追加
        EurekaServerApplicationクラスにJavadocコメントを追加しました。クラスとメソッドの役割や使用方法についての詳細な説明を提供しています。

--- service-registry/src/main/java/undecided/serviceregistory/EurekaServerApplication.java
+++ service-registry/src/main/java/undecided/serviceregistory/EurekaServerApplication.java
@@ -4,11 +4,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * これはEureka Serverアプリケーションのメインクラスです。
+ * <p>
+ * このアプリケーションはNetflix Eurekaを使用してサービスレジストリとして機能します。
+ *
+ * @SpringBootApplicationアノテーションはこのクラスをSpring Bootアプリケーションとしてマークします。
+ * @EnableEurekaServerアノテーションはアプリケーションにEurekaサーバーを有効にします。
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
 
+  /**
+   * Eureka サーバーアプリケーションのエントリーポイント。
+   * <p>
+   * このメソッドは Spring Boot の SpringApplication.run を呼び出して、 アプリケーションを起動します。
+   *
+   * @param args 起動時にアプリケーションに渡されるコマンドライン引数
+   */
   public static void main(String[] args) {
     SpringApplication.run(EurekaServerApplication.class, args);
   }
+
 }
