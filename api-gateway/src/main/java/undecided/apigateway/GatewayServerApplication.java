f42ebce8d66c7dd2b4cacaf5b7d16e8577dd4588
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2024-08-30 11:27:42 +0900

    Add JavaDoc comments to GatewayServerApplication
        主要なクラスとメソッドに対してJavaDocコメントを追加しました。これにより、コードの読みやすさとメンテナビリティが向上します。

--- api-gateway/src/main/java/undecided/apigateway/GatewayServerApplication.java
+++ api-gateway/src/main/java/undecided/apigateway/GatewayServerApplication.java
@@ -7,10 +7,22 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
+/**
+ * GatewayServerApplicationは、Spring Bootアプリケーションのエントリーポイントです。
+ * <p>
+ * このアプリケーションはゲートウェイサーバーとして機能し、提供された設定に基づいて 入ってくるリクエストを適切なサービスにルーティングします。
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GatewayServerApplication {
 
+  /**
+   * メインメソッドはSpring Bootアプリケーションのエントリーポイントとして機能します。
+   * <p>
+   * これはGatewayServerApplicationを初期化し、実行します。
+   *
+   * @param args アプリケーションに渡されるコマンドライン引数。
+   */
   public static void main(String[] args) {
     SpringApplication.run(GatewayServerApplication.class, args);
   }
