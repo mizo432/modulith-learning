c5f7fc82a269944830103643d72a1e903863c392
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2024-08-30 11:43:27 +0900

    Update ModulithDemoApplication with detailed Javadoc comments
        Javadocコメントを追加して、アプリケーション名とサーバーポートの説明を詳述。プロパティの注入に誤記修正も行い、コードの理解を容易にしました。

--- backend/src/main/java/undecided/erp/ModulithDemoApplication.java
+++ backend/src/main/java/undecided/erp/ModulithDemoApplication.java
@@ -7,30 +7,46 @@ import org.springframework.context.event.ContextStartedEvent;
 import org.springframework.context.event.EventListener;
 import undecided.erp.shared.applicatoion.ApplicationInfoInitializer;
 
+/**
+ * Modulith Demoアプリケーションのメインクラス。
+ * <p>
+ * このクラスはSpring Bootアプリケーションの初期化と起動を行います。また、 アプリケーションコンテキストが開始されたイベントを処理し、
+ * アプリケーション名やサーバーポートなど、アプリケーション固有の情報を初期化します。
+ */
 @SpringBootApplication
 public class ModulithDemoApplication {
 
   /**
-   * Starts the Modulith Demo application.
+   * アプリケーションの名前を示す変数。この値はSpringのプロパティファイルから取得される。 具体的には、`spring.application.name`プロパティに対応する。
+   * この変数は、アプリケーションの初期化と終了時に使用されるログやその他の情報に利用される。
+   */
+  @Value("${spring.application.name}")
+  private String applicationName;
+  /**
+   * 変数は、サーバーがリッスンするポート番号を格納します。 この値はSpringのプロパティファイルから取得され、アプリケーションの初期化時に設定されます。
+   * <p>
+   * 具体的には`server.port`プロパティに対応します。サーバーが特定のポートで起動するように構成できます。
+   */
+  @Value("${server.port}")
+  private String serverPort;
+
+  /**
+   * Modulith Demoアプリケーションを開始します。
    *
-   * @param args the command line arguments
+   * @param args コマンドライン引数
    */
   public static void main(String[] args) {
     SpringApplication.run(ModulithDemoApplication.class, args);
   }
 
-  @Value("spring.application.name")
-  private String applicationNane;
-  @Value("server.port")
-  private String serverPort;
-
   /**
-   * @param ctxStartEvt このメソッドをトリガーする ContextStartedEvent
-   * @see ApplicationInfoInitializer#initialize(String, Long)
+   * ContextStartedEventを処理し、アプリケーション名やサーバーポートなどの アプリケーション固有の情報を初期化します。
+   *
+   * @param ctxStartEvt コンテキストの開始を表すイベント
    */
   @EventListener
   public void handleContextRefreshEvent(ContextStartedEvent ctxStartEvt) {
-    ApplicationInfoInitializer.initialize(applicationNane, Long.valueOf(serverPort));
+    ApplicationInfoInitializer.initialize(applicationName, Long.valueOf(serverPort));
 
   }
 
