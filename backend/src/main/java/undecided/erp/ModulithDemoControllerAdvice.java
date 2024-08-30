cefe24a95d86f15ff378eb16b597cb2e7c0438c2
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2024-08-30 11:45:05 +0900

    コメントを日本語に翻訳
        ModulithDemoControllerのためのコントローラーアドバイスのコメントを英語から日本語に翻訳しました。これにより、コードベースが日本語に統一され、より一貫性のあるドキュメントが提供されます。

--- backend/src/main/java/undecided/erp/ModulithDemoControllerAdvice.java
+++ backend/src/main/java/undecided/erp/ModulithDemoControllerAdvice.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Controller advice for ModulithDemoController.
+ * ModulithDemoControllerのためのコントローラーアドバイス。
  */
 @ControllerAdvice(annotations = {RestController.class})
 public class ModulithDemoControllerAdvice {
