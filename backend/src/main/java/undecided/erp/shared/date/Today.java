551b78e448aec79bbbd134341763b52a9e010b5b
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2024-12-24 20:59:59 +0900

    Add JavaDoc comments to Today class for better clarity
    今日の日付を表す `Today` クラスに JavaDoc コメントを追加しました。 クラスの目的、不変性、及び提供されるユーティリティメソッドについて簡潔に説明しています。これにより、コードの読みやすさと保守性が向上します。

--- backend/src/main/java/undecided/erp/shared/date/Today.java
+++ backend/src/main/java/undecided/erp/shared/date/Today.java
@@ -5,11 +5,14 @@ import java.time.format.DateTimeFormatter;
 import org.springframework.lang.NonNull;
 import undecided.erp.common.dateProvider.DateProvider;
 
+/**
+ * {@link LocalDate} をカプセル化したレコードで、今日の日付を表します。
+ * <p>
+ * このクラスは、カプセル化された日付を操作するためのユーティリティメソッド（1か月を加算したり、 現在の日付を元に新しいインスタンスを生成するなど）を提供します。
+ * <p>
+ * {@code Today} レコードは、不変性を確保し、日付処理に特化した機能を提供します。
+ */
 public record Today(LocalDate value) {
-
-  public LocalDate addOneMonth() {
-    return value.plusMonths(1);
-  }
 
   public static Today of(@NonNull LocalDate value) {
     return new Today(value);
@@ -19,6 +22,10 @@ public record Today(LocalDate value) {
   public static Today newInstance() {
     return new Today(DateProvider.currentLocalDate());
 
+  }
+
+  public LocalDate addOneMonth() {
+    return value.plusMonths(1);
   }
 
   public int nextMonthInteger() {
