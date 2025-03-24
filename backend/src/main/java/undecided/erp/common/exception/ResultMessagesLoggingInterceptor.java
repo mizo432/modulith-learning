27b206644ca3f36d1de68ce9a9b011d1aadbcc7a
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2025-03-24 20:59:37 +0900

    ResultMessagesLoggingInterceptorクラスを追加
    例外処理を行うResultMessagesLoggingInterceptorクラスを新規作成。ResultMessagesNotificationExceptionのログ出力機能を提供し、AOPを用いたメソッドインターセプトを実現する。

---
+++ backend/src/main/java/undecided/erp/common/exception/ResultMessagesLoggingInterceptor.java
@@ -0,0 +1,63 @@
+package undecided.erp.common.exception;
+
+import lombok.Setter;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.beans.factory.InitializingBean;
+
+public class ResultMessagesLoggingInterceptor implements MethodInterceptor, InitializingBean {
+
+  private final ThreadLocal<MethodInvocation> startingPoint = new ThreadLocal<>();
+  @Setter
+  private ExceptionLogger exceptionLogger = null;
+
+  public ResultMessagesLoggingInterceptor() {
+  }
+
+  public Object invoke(MethodInvocation invocation)
+      throws Throwable, ResultMessagesNotificationException {
+    if (this.startingPoint.get() == null) {
+      this.startingPoint.set(invocation);
+    }
+
+    Object e;
+    try {
+      e = invocation.proceed();
+    } catch (ResultMessagesNotificationException var6) {
+      if (this.isStartingPoint(invocation)) {
+        this.logResultMessagesNotificationException(var6);
+      }
+
+      throw var6;
+    } finally {
+      if (this.isStartingPoint(invocation)) {
+        this.startingPoint.remove();
+      }
+
+    }
+
+    return e;
+  }
+
+  public void afterPropertiesSet() throws Exception {
+    if (this.exceptionLogger == null) {
+      this.exceptionLogger = new ExceptionLogger(this.getClass().getName());
+      this.exceptionLogger.afterPropertiesSet();
+    }
+
+  }
+
+  protected boolean isStartingPoint(MethodInvocation invocation) {
+    return this.startingPoint.get() == invocation;
+  }
+
+  protected void logResultMessagesNotificationException(ResultMessagesNotificationException e) {
+    this.exceptionLogger.warn(e);
+  }
+
+  protected ExceptionLogger getExceptionLogger() {
+    return this.exceptionLogger;
+
+  }
+
+}
