53a1dde7d97dbb4bcce82de8dcc2dd15938d1663
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2025-04-06 04:52:12 +0900

    HttpSessionイベントのロギング機能を追加
    HttpSessionに関連するライフサイクルおよび属性変更イベントをロギングする機能を追加しました。セッションの作成、破棄、アクティベーション、パッシベーション、属性の追加・変更・削除イベントを監視し、デバッグログまたはトレースログに記録します。主にセッション操作のトレースとデバッグ情報の収集を目的とした変更です。

--- backend/src/main/java/undecided/erp/common/web/logging/HttpSessionEventLoggingListener.java
+++ backend/src/main/java/undecided/erp/common/web/logging/HttpSessionEventLoggingListener.java
@@ -9,6 +9,20 @@ import jakarta.servlet.http.HttpSessionListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * HttpSessionEventLoggingListener は、HttpSession に関連するライフサイクルイベントおよび属性変更イベントのロギングを提供するクラスです。
+ * <p>
+ * このクラスは、HttpSessionListener、HttpSessionAttributeListener、HttpSessionActivationListener
+ * インターフェースを実装しています。
+ * セッションの作成、破棄、アクティベーション、パッシベーション、属性の追加・変更・削除などのイベントを監視し、それらをデバッグログまたはトレースログに記録します。
+ * <p>
+ * 主に以下のイベントを処理します: 1. セッションのライフサイクルイベント: - セッションの作成と破棄 - セッションのアクティベーションとパッシベーション 2.
+ * セッション属性の変更イベント: - 属性の追加、削除、置換
+ * <p>
+ * フィールド: - logger: イベントの情報をロギングするための Logger インスタンス。
+ * <p>
+ * 利用用途: アプリケーション内のセッション操作をトレースし、デバッグ情報を収集するために使用されます。
+ */
 public class HttpSessionEventLoggingListener implements HttpSessionListener,
     HttpSessionAttributeListener, HttpSessionActivationListener {
 
@@ -18,6 +32,14 @@ public class HttpSessionEventLoggingListener implements HttpSessionListener,
   public HttpSessionEventLoggingListener() {
   }
 
+  /**
+   * セッションが非アクティブ状態に遷移した際に呼び出されるメソッドです。
+   * <p>
+   * このメソッドは、セッションの非アクティブ化イベントに関する情報をデバッグログに記録します。
+   *
+   * @param se セッション非アクティブ化イベント情報を格納した HttpSessionEvent オブジェクト -
+   * このオブジェクトにはイベントがトリガーされたセッション情報が含まれます。
+   */
   public void sessionWillPassivate(HttpSessionEvent se) {
     if (logger.isDebugEnabled()) {
       HttpSession session = se.getSession();
@@ -26,6 +48,13 @@ public class HttpSessionEventLoggingListener implements HttpSessionListener,
 
   }
 
+  /**
+   * セッションがアクティブ状態に遷移した際に呼び出されるメソッドです。
+   * <p>
+   * このメソッドは、セッションの活性化イベントに関する情報をデバッグログに記録します。
+   *
+   * @param se セッション活性化イベント情報を格納した HttpSessionEvent オブジェクト - このオブジェクトにはイベントがトリガーされたセッション情報が含まれます。
+   */
   public void sessionDidActivate(HttpSessionEvent se) {
     if (logger.isDebugEnabled()) {
       HttpSession session = se.getSession();
@@ -34,6 +63,14 @@ public class HttpSessionEventLoggingListener implements HttpSessionListener,
 
   }
 
+  /**
+   * セッションに属性が追加された際に呼び出されるメソッドです。
+   * <p>
+   * このメソッドは、セッションにおける属性追加イベントをデバッグログに記録します。
+   *
+   * @param se 属性追加イベント情報を格納した HttpSessionBindingEvent オブジェクト -
+   * イベントがトリガーされたセッション情報や、追加された属性名および値が含まれます。
+   */
   public void attributeAdded(HttpSessionBindingEvent se) {
     if (logger.isDebugEnabled()) {
       HttpSession session = se.getSession();
@@ -43,6 +80,14 @@ public class HttpSessionEventLoggingListener implements HttpSessionListener,
 
   }
 
+  /**
+   * セッションから属性が削除された際に呼び出されるメソッドです。
+   * <p>
+   * このメソッドは、セッションにおける属性削除イベントをデバッグログに記録します。
+   *
+   * @param se 属性削除イベント情報を格納した HttpSessionBindingEvent オブジェクト -
+   * イベントがトリガーされたセッション情報や、削除された属性名および値が含まれます。
+   */
   public void attributeRemoved(HttpSessionBindingEvent se) {
     if (logger.isDebugEnabled()) {
       HttpSession session = se.getSession();
@@ -52,6 +97,14 @@ public class HttpSessionEventLoggingListener implements HttpSessionListener,
 
   }
 
+  /**
+   * セッション属性が置き換えられた際に呼び出されるメソッドです。
+   * <p>
+   * このメソッドは、セッションにおける属性置換イベントをトレースログに記録します。
+   *
+   * @param se 属性置換イベント情報を格納した HttpSessionBindingEvent オブジェクト -
+   * イベントがトリガーされたセッション情報や、置き換えられた属性名および値が含まれます。
+   */
   public void attributeReplaced(HttpSessionBindingEvent se) {
     if (logger.isTraceEnabled()) {
       HttpSession session = se.getSession();
@@ -61,6 +114,13 @@ public class HttpSessionEventLoggingListener implements HttpSessionListener,
 
   }
 
+  /**
+   * セッションが作成された際に呼び出されるメソッドです。
+   * <p>
+   * セッションの作成イベントの情報をデバッグログに記録します。
+   *
+   * @param se セッションイベント情報を格納した HttpSessionEvent オブジェクト
+   */
   public void sessionCreated(HttpSessionEvent se) {
     if (logger.isDebugEnabled()) {
       HttpSession session = se.getSession();
@@ -69,6 +129,13 @@ public class HttpSessionEventLoggingListener implements HttpSessionListener,
 
   }
 
+  /**
+   * セッションが破棄された際に呼び出されるメソッドです。
+   * <p>
+   * セッションの破棄イベントの情報をデバッグログに記録します。
+   *
+   * @param se セッションイベント情報を格納した HttpSessionEvent オブジェクト
+   */
   public void sessionDestroyed(HttpSessionEvent se) {
     if (logger.isDebugEnabled()) {
       HttpSession session = se.getSession();
