d5e8127b67c6a4709ec0880ce8db1f4bddafcc25
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2025-05-05 00:10:24 +0900

    アプリケーション層にアノテーションを追加
    package-info.javaに@ApplicationServiceRingアノテーションを追加しました。これにより、アプリケーション層が明示的に定義され、構造の可読性と理解が向上します。

--- backend/src/main/java/undecided/erp/scrum/application/package-info.java
+++ backend/src/main/java/undecided/erp/scrum/application/package-info.java
@@ -1,5 +1,9 @@
 /**
- * アプリケーション層のパッケージ。 このパッケージはクリーンアーキテクチャ/オニオンアーキテクチャにおけるアプリケーション層を実装し、
- * ドメインオブジェクトを操作するためのユースケースを提供します。
+ * アプリケーション層のパッケージ。
+ * <p>
+ * このパッケージはクリーンアーキテクチャ/オニオンアーキテクチャにおけるアプリケーション層を実装し、 ドメインオブジェクトを操作するためのユースケースを提供します。
  */
+@ApplicationServiceRing
 package undecided.erp.scrum.application;
+
+import org.jmolecules.architecture.onion.classical.ApplicationServiceRing;
