8b2fe5e33a22b73cc4b11ef9adbb7e7e9663f6ca
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2025-12-07 19:48:16 +0900

    feat: add task executor using virtual threads
    Introduced a new `taskExecutor` bean in `ApplicationConfig` that utilizes Java's virtual threads. This enables efficient parallel task execution, improving scalability and performance.

--- backend/src/main/java/undecided/erp/shared/applicatoion/ApplicationConfig.java
+++ backend/src/main/java/undecided/erp/shared/applicatoion/ApplicationConfig.java
@@ -1,5 +1,7 @@
 package undecided.erp.shared.applicatoion;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import undecided.erp.common.exception.ExceptionLogger;
@@ -12,5 +14,15 @@ public class ApplicationConfig {
   @Bean
   public ExceptionLogger exceptionLogger() {
     return new ExceptionLogger(EXCEPTION_LOGGER_NAME);
+  }
+
+  /**
+   * タスク専用の実行サービス(Executor)を提供するメソッド。 このメソッドで生成されたExecutorは仮想スレッド(Virtual Thread)を使用してタスクを並列実行します。
+   *
+   * @return 新しい仮想スレッドベースのタスク実行サービス
+   */
+  @Bean
+  public Executor taskExecutor() {
+    return Executors.newVirtualThreadPerTaskExecutor();
   }
 }
