d01610f96650d59c61381643c0c594a3b37e9327
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2026-05-23 08:00:27 +0900

    docs: ResultMessageクラスのJavaDocを更新
    ResultMessageクラスのJavaDocを全面的に改善しました。メソッドやパラメータの説明を日本語で明確化し、クラスの目的や使用方法がより分かりやすくなるよう修正しました。

--- backend/src/main/java/undecided/shared/common/message/ResultMessage.java
+++ backend/src/main/java/undecided/shared/common/message/ResultMessage.java
@@ -4,14 +4,17 @@ import java.util.Arrays;
 import org.jspecify.annotations.NonNull;
 import undecided.shared.common.primitiveOld.Objects2;
 
+/** メッセージを格納するためのクラス。メッセージコード、引数、デフォルトメッセージを保持します。 */
 public record ResultMessage(String code, Object[] args, String text) {
 
   /**
-   * Constructor.
+   * ResultMessageのコンストラクタです。
    *
-   * @param code message code
-   * @param args replacement values of message format
-   * @param text default message
+   * <p>指定されたメッセージコード、引数、メッセージテキストを用いて初期化します。
+   *
+   * @param code メッセージコード。この値はnullではいけません。
+   * @param args メッセージフォーマットの置換値。nullが指定された場合は空の配列に置換されます。
+   * @param text メッセージテキスト。この値は任意で指定可能です。
    */
   public ResultMessage(String code, Object[] args, String text) {
     this.code = code;
@@ -20,30 +23,31 @@ public record ResultMessage(String code, Object[] args, String text) {
   }
 
   /**
-   * create <code>ResultMessage</code> instance which has the given code and args<br>
+   * 指定されたメッセージコードおよび可変長引数を使用して、ResultMessageのインスタンスを生成します。
    *
-   * <p><code>text</code> is <code>null</code>
-   *
-   * @param code message code (must not be null)
-   * @param args replacement values of message format
-   * @return ResultMessage instance
+   * @param code メッセージコード。この値はnullではいけません。
+   * @param args メッセージフォーマットの置換値。nullが指定された場合は空の配列に置換されます。
+   * @return 生成されたResultMessageインスタンス
    */
   public static ResultMessage fromCode(@NonNull String code, Object... args) {
     return new ResultMessage(code, args, null);
   }
 
   /**
-   * create <code>ResultMessage</code> instance which has the given text<br>
+   * 指定されたテキストを使用してResultMessageのインスタンスを生成します。
    *
-   * <p><code>code</code> is <code>null</code>
-   *
-   * @param text message tet (must not be null)
-   * @return ResultMessage instance
+   * @param text メッセージテキスト。この値はnullではいけません。
+   * @return 生成されたResultMessageインスタンス
    */
   public static ResultMessage fromText(@NonNull String text) {
     return new ResultMessage(null, Objects2.EMPTY_ARRAY, text);
   }
 
+  /**
+   * このメソッドはResultMessageオブジェクトの文字列表現を返します。
+   *
+   * @return メッセージコード、引数の配列、テキストを含むResultMessageオブジェクトの文字列表現
+   */
   @Override
   @NonNull
   public String toString() {
