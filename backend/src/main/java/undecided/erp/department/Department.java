89474a1084c8677b6a96a23fc47c6bdadd67ecf3
Mizoguchi <hachirouuemon.mizoguchi@gmail.com>
2026-05-29 10:45:24 +0900

    ユーティリティを追加した

--- backend/src/main/java/undecided/erp/department/Department.java
+++ backend/src/main/java/undecided/erp/department/Department.java
@@ -1,21 +1,27 @@
 package undecided.erp.department;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.jspecify.annotations.NonNull;
 import undecided.erp.common.entity.SnowflakeId;
 import undecided.erp.department.internal.DepartmentCode;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Department {
-    private DepartmentCode departmentCode = DepartmentCode.EMPTY;
+    @Id
     private SnowflakeId id = SnowflakeId.EMPTY;
+    @Column(unique = true, nullable = false, length = 13, name = "code")
+    private DepartmentCode code = DepartmentCode.EMPTY;
 
-    public DepartmentCode getDepartmentCode() {
-        return departmentCode;
+    public Department update(@NonNull Department department) {
+        return this;
     }
 
-    public Department update(Department department) {
-        return null;
-    }
-
-    public SnowflakeId getId() {
-        return id;
-    }
 }
