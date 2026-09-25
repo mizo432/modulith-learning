e5858059763be89b21438f95499be9a1b9aa4ce6
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2026-09-25 11:47:20 +0900

    chore: Hibernateの@Commentアノテーションを削除し@Table/@Columnに統合
    @Columnと@Tableのcomment属性を使用するように修正し、@Commentアノテーションを削除しました。コードの簡潔性と保守性を向上させる変更です。

--- backend/src/main/java/undecided/erp/shared/address/spi/Prefecture.java
+++ backend/src/main/java/undecided/erp/shared/address/spi/Prefecture.java
@@ -10,65 +10,55 @@ import java.time.LocalDate;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.annotations.Comment;
 
 @Getter
 @Setter
 @ToString
 @Entity
-@Table(name = "prefectures")
-@Comment("prefecture table")
+@Table(name = "prefectures", comment = "prefecture table")
 public class Prefecture {
 
   @Id
-  @Comment("Unique identifier for the prefecture")
-  @Column(name = "prefecture_id", nullable = false)
+  @Column(
+      name = "prefecture_id",
+      comment = "Unique identifier for the prefecture",
+      nullable = false)
   private Long id;
 
   @Size(max = 2)
   @NotNull
-  @Comment("Prefecture code")
-  @Column(name = "prefecture_code", nullable = false, length = 2)
+  @Column(name = "prefecture_code", comment = "Prefecture code", nullable = false, length = 2)
   private String prefectureCode;
 
   @Size(max = 6)
   @NotNull
-  @Comment("Legal code")
-  @Column(name = "lg_code", nullable = false, length = 6)
+  @Column(name = "lg_code", comment = "Legal code", nullable = false, length = 6)
   private String lgCode;
 
   @Size(max = 10)
   @NotNull
-  @Comment("Prefecture name")
-  @Column(name = "pref_name", nullable = false, length = 10)
+  @Column(name = "pref_name", comment = "Prefecture name", nullable = false, length = 10)
   private String prefName;
 
   @Size(max = 50)
   @NotNull
-  @Comment("Prefecture kana")
-  @Column(name = "pref_kana", nullable = false, length = 50)
+  @Column(name = "pref_kana", comment = "Prefecture kana", nullable = false, length = 50)
   private String prefKana;
 
   @Size(max = 50)
   @NotNull
-  @Comment("Prefecture romaji")
-  @Column(name = "pref_roma", nullable = false, length = 50)
+  @Column(name = "pref_roma", comment = "Prefecture romaji", nullable = false, length = 50)
   private String prefRoma;
 
   @NotNull
-  @Comment("Effective date")
-  @Column(name = "effective_date", nullable = false)
+  @Column(name = "effective_date", comment = "Effective date", nullable = false)
   private LocalDate effectiveDate;
 
   @NotNull
-  @Comment("Abolition date")
-  @Column(name = "abolition_data", nullable = false)
+  @Column(name = "abolition_data", comment = "Abolition date", nullable = false)
   private LocalDate abolitionData;
 
   @Size(max = 256)
-  @Comment("Remarks")
-  @Column(name = "remarks", length = 256)
+  @Column(name = "remarks", comment = "Remarks", length = 256)
   private String remarks;
-
-
 }
